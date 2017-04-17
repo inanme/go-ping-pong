f87643d9b40fb253c98eef723d8c4c8593dcb7fc
mert inan <inanme@gmail.com>
2017-04-17 21:07:29 +0100

    golden

---
+++ src/hello/main.go
@@ -0,0 +1,45 @@
+package main
+
+//filename does not matter, folder package name is the most important thing
+//in the project root level
+// export GOPATH=$PWD && go build hello
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	ping = "ping"
+	pong = "pong"
+)
+
+func replyMessage(message string) string {
+	if message == ping {
+		return pong
+	} else if message == pong {
+		return ping
+	} else {
+		return ""
+	}
+}
+
+func actor(channel chan string, init string) {
+	if len(replyMessage(init)) > 0 {
+		channel <- replyMessage(init)
+	}
+	for true {
+		message := <-channel
+		fmt.Println(message)
+		channel <- replyMessage(message)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	channel := make(chan string)
+	go actor(channel, ping)
+	go actor(channel, "")
+
+	time.Sleep(10 * time.Second)
+}
